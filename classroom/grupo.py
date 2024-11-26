411cfedd09fbc93baa174337d7975700e36996ee
JackelinBlanquiceth <glozanob@unal.edu.co>
2024-11-26 12:15:09 -0500

    Update grupo.py

--- classroom/grupo.py
+++ classroom/grupo.py
@@ -14,8 +14,8 @@ class Grupo:
         for x in kwargs.values():
             self._asignaturas.append(Asignatura(x))
 
-    def agregarAlumno(self, alumno, lista=[]):
-        if lista == []:
+    def agregarAlumno(self, alumno, lista=None):
+        if lista == None:
             lista = []           
         lista.append(alumno)
         self.listadoAlumnos = self.listadoAlumnos + lista
